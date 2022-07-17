5a95c8b5470184b44d752b8313e9d53aecb46a0a
Gginne <23042760+Gginne@users.nonreply.github.com>
2022-07-17 01:25:33 -0400

    Basic MySQL implementation with SQLAlchemy

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
-from flask import Flask, request, jsonify
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from database import init_db
+
+db = SQLAlchemy()
 
 
 def create_app():
-    app = Flask(__name__)
-    
-    # Register server routes
-    
-    # Register middlewares
-    
-    # Exception handlers
-    
+    """Construct the core application."""
+    app = Flask(__name__, instance_relative_config=False)
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
     return app
 
 def start_server():
-    
     app = create_app()
     app.run(host='0.0.0.0', port=105)
     
-    
+
 if __name__ == '__main__':
+    init_db()
     start_server()
